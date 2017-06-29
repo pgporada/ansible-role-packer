fe50a8d62e97cdac89344812ca98db03b02e73ff
Phil Porada <philporada@gmail.com>
2017-06-28 23:36:38 -0400

    Fixed update automagic script

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-[![](https://img.shields.io/badge/Supports%20Packer%20Version-1.0.2-blue.svg)](https://github.com/hashicorp/packer/blob/v1.0.2/CHANGELOG.md)
+[![License](https://img.shields.io/badge/license-GPLv3-brightgreen.svg)](LICENSE)
 [![](https://img.shields.io/badge/Supports%20Packer%20Version-1.0.2-blue.svg)](https://github.com/hashicorp/packer/blob/v1.0.2/CHANGELOG.md)
 
 # Overview: pgporada.packer
--- update_automagic.sh
+++ update_automagic.sh
@@ -19,6 +19,6 @@ echo "+) Updating occurrences of 'packer_version: #.#.#'"
 egrep -r "packer_version: [0-9]+\.[0-9]+\.[0-9]"
 
 echo "+) Updating the badge in the README.md"
-sed -i "/img.shields.io/c\[\![](https://img.shields.io/badge/Supports%20Packer%20Version-${VERSION}-blue.svg)](https://github.com/hashicorp/packer/blob/v${VERSION}/CHANGELOG.md)" README.md
+sed -i "/img.shields.io\/badge\/Supports/c\[\![](https://img.shields.io/badge/Supports%20Packer%20Version-${VERSION}-blue.svg)](https://github.com/hashicorp/packer/blob/v${VERSION}/CHANGELOG.md)" README.md
 
 
